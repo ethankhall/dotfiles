ec2554ce9437dc8bd4d9e48f56d01443684bf5e5
Ethan Hall <ethan@ehdev.io>
2014-09-13 23:04:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,6 @@ brew doctor
 brew install zsh zsh-completions zsh-lovers zsh-syntax-highlighting reattach-to-user-namespace gradle groovy tmux ssh-copy-id tree watch htop-osx ansible wget
 sudo chsh -s `which zsh` `whoami`
 
-read -p "Install VirtualBox: https://www.virtualbox.org/wiki/Downloads"
 read -p "Press [Enter] key to continue..."
 
 # Install Pathogen for VIM
