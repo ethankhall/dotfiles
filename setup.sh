4366ee52af53ef11fefa402f83e820695fcc4334
Ethan Hall <ethan@ehdev.io>
2015-08-18 23:17:41 -0700

    Fixing setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "$(uname)" == "Darwin" ]]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew doctor
     brew install ansible
     ansible-galaxy install --ignore-errors --role-file requirements.yml
     ansible-playbook -i "localhost," -c local playbook.yml
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     # Do something under Linux platform
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    echo "Nope."
+    echo ""
 fi
