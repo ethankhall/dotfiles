473977619c4448453c32859cb0abb2c35de26f2f
Ethan Hall <ethan@ehdev.io>
2015-08-18 22:49:21 -0700

    Migrated to ansible

--- random_system_changes.sh
+++
@@ -1,5 +0,0 @@
-defaults write NSGlobalDomain InitialKeyRepeat -int 12
-defaults write NSGlobalDomain KeyRepeat -int 0.01
-defaults write -g ApplePressAndHoldEnabled -bool false
-
-#https://apple.stackexchange.com/questions/115562/how-do-i-disable-the-minimize-command-m-shortcut-in-mavericks
--- setup.sh
+++ setup.sh
@@ -1,72 +1,11 @@
-#!/bin/bash
-
-echo "Installing Brew"
-PERSONAL_DIR=~/workspace/personal
-
-#Install brew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew doctor
-brew install zsh zsh-completions zsh-lovers zsh-syntax-highlighting reattach-to-user-namespace \
-    gradle groovy tmux ssh-copy-id tree watch htop-osx ansible wget \
-    git dnsmasq npm jq
-    
-npm install -g gulp-cli bower
-    
-sudo chsh -s `which zsh` `whoami`
-
-gem install tmuxinator
-
-brew install caskroom/cask/brew-cask
-
-brew cask install google-chrome iterm2 1password vagrant vagrant-manager \
-    intellij-idea caskroom/versions/java7 sizeup marked bartender istat-menus \
-    virtualbox vmware-fusion caffeine flux dropbox alfred
-
-# Install Pathogen for VIM
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-#Install nerdtree
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-
-#Clone Personal repo
-echo "Cloneing personal repo"
-DOT_FILES=$PERSONAL_DIR/dotfiles
-mkdir -p $PERSONAL_DIR
-git clone https://github.com/ethankhall/dotfiles.git $DOT_FILES
-
-echo "mapping personal files"
-ln -s $DOT_FILES/.gitconfig ~/.gitconfig
-ln -s $DOT_FILES/.tmux.conf ~/.tmux.conf
-ln -s $DOT_FILES/.vimrc ~/.vimrc
-ln -s $DOT_FILES/.tmux ~/.tmux
-
-echo "Cloning personal zprezto"
-git clone --recursive https://github.com/ethankhall/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
 if [ "$(uname)" == "Darwin" ]; then
-    echo "Setting up custom Mac settings"
-    defaults write NSGlobalDomain InitialKeyRepeat -int 12
-    defaults write NSGlobalDomain KeyRepeat -int 0.01
-    defaults write -g ApplePressAndHoldEnabled -bool false
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew doctor
+    brew install ansible
+    ansible-galaxy install --ignore-errors --role-file requirements.yml
+    ansible-playbook -i "localhost," -c local playbook.yml
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under Linux platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo "Nope."
 fi
-
-sudo wget https://github.com/fncll/vimstuff/raw/master/powerline-fonts/Andale_Mono-Powerline.ttf --directory-prefix=/Library/Fonts/
-sudo wget https://github.com/fncll/vimstuff/raw/master/powerline-fonts/SourceCodePro-Black-Powerline.otf --directory-prefix=/Library/Fonts/
-sudo wget https://github.com/fncll/vimstuff/raw/master/powerline-fonts/SourceCodePro-Bold-Powerline.otf --directory-prefix=/Library/Fonts/
-sudo wget https://github.com/fncll/vimstuff/raw/master/powerline-fonts/SourceCodePro-ExtraLight-Powerline.otf --directory-prefix=/Library/Fonts/
-sudo wget https://github.com/fncll/vimstuff/raw/master/powerline-fonts/SourceCodePro-Light-Powerline.otf --directory-prefix=/Library/Fonts/
-sudo wget https://github.com/fncll/vimstuff/raw/master/powerline-fonts/SourceCodePro-Regular-Powerline.otf --directory-prefix=/Library/Fonts/
-sudo wget https://github.com/fncll/vimstuff/raw/master/powerline-fonts/SourceCodePro-Semibold-Powerline.otf --directory-prefix=/Library/Fonts/
-
-cat > zshconfig.sh << END
-#!/usr/bin/env zsh
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-END
-
-chmod +x zshconfig.sh
-`which zsh` ./zshconfig.sh
